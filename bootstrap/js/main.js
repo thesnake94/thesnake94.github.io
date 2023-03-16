c4639f8f72412a238da47b69a6d99bdd0e2efd77
thesnake94 <dromdhane@guardiaschool.fr>
2023-03-16 22:38:33 +0100

    navbar

---
+++ bootstrap/js/main.js
@@ -0,0 +1,9 @@
+window.onscroll = function () {
+	if (window.innerWidth > 991) {
+		if (window.pageYOffset > 100) {
+			document.querySelector('nav').classList.add('sticky');
+		} else {
+			document.querySelector('nav').classList.remove('sticky');
+		}
+	}
+};
