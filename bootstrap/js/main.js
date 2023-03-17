2c96d4087baf57b12fae34e3392f46ad17ca5702
thesnake94 <dromdhane@guardiaschool.fr>
2023-03-17 23:55:31 +0100

    timeline animate

--- bootstrap/js/main.js
+++ bootstrap/js/main.js
@@ -19,3 +19,29 @@ window.addEventListener('scroll', function () {
 		scrollToTop.classList.remove('show');
 	}
 });
+
+//
+// element container-time dans la timeline qui apparaissent un par un
+// Sélectionnez tous les éléments avec la classe "container-time"
+const containerTimes = document.querySelectorAll('.container-time');
+
+// Options pour l'Intersection Observer
+const options = {
+	rootMargin: '0px',
+	threshold: 0.5,
+};
+
+// Créez une instance de l'Intersection Observer avec une fonction de callback
+const observer = new IntersectionObserver(function (entries, observer) {
+	entries.forEach((entry) => {
+		// Si l'élément est dans la vue de l'utilisateur, ajoutez la classe "visible" à l'élément
+		if (entry.isIntersecting) {
+			entry.target.classList.add('visible');
+		}
+	});
+}, options);
+
+// Pour chaque élément avec la classe "container-time", observez l'élément
+containerTimes.forEach((containerTime) => {
+	observer.observe(containerTime);
+});
